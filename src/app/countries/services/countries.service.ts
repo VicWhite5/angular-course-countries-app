d5b8f582a780c2deda21c4428a871c39beacfa98
Victor White <victorblanco281201@gmail.com>
2024-08-02 12:31:55 -0400

    Done until video

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, map, Observable, of } from 'rxjs';
+import { catchError, delay, map, Observable, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({
@@ -10,6 +10,11 @@ export class CountriesService {
   private apiUrl = 'https://restcountries.com/v3.1';
 
   constructor(private http: HttpClient) {}
+
+  private getCountriesRequest(url: string): Observable<Country[]> {
+    // return this.http.get<Country[]>(url).pipe(catchError((error) => of([])), delay(2000));
+    return this.http.get<Country[]>(url).pipe(catchError((error) => of([])));
+  }
 
   searchCountryByAlphaCode(code: string): Observable<Country | null> {
     const url: string = `${this.apiUrl}/alpha/${code}`;
@@ -22,19 +27,16 @@ export class CountriesService {
 
   searchCapital(term: string): Observable<Country[]> {
     const url: string = `${this.apiUrl}/capital/${term}`;
-
-    return this.http.get<Country[]>(url).pipe(catchError((error) => of([])));
+    return this.getCountriesRequest(url);
   }
 
   searchCountry(term: string): Observable<Country[]> {
     const url: string = `${this.apiUrl}/name/${term}`;
-
-    return this.http.get<Country[]>(url).pipe(catchError((error) => of([])));
+    return this.getCountriesRequest(url);
   }
 
   searchRegion(term: string): Observable<Country[]> {
     const url: string = `${this.apiUrl}/region/${term}`;
-
-    return this.http.get<Country[]>(url).pipe(catchError((error) => of([])));
+    return this.getCountriesRequest(url);
   }
 }
--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,14 +1,17 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { debounceTime, Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
-  styles: ``
+  styles: ``,
 })
-export class SearchBoxComponent {
-
+export class SearchBoxComponent implements OnInit, OnDestroy {
   // @ViewChild('txtSearchInput')
   // public searchInput!: ElementRef<HTMLInputElement>;
+
+  private dbouncer: Subject<string> = new Subject<string>();
+  private dbouncerSubscription?: Subscription;
 
   @Input()
   public placeholder: string = '';
@@ -16,8 +19,31 @@ export class SearchBoxComponent {
   @Output()
   public onSearch: EventEmitter<string> = new EventEmitter<string>();
 
+  @Output()
+  public onDebounce: EventEmitter<string> = new EventEmitter<string>();
+
+  ngOnInit(): void {
+    this.dbouncerSubscription = this.dbouncer
+    .pipe(
+      debounceTime(300)
+    )
+    .subscribe((value) => {
+      this.onDebounce.emit(value);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.dbouncerSubscription?.unsubscribe();
+  }
+
   searchValue(term: string) {
     // const term: string = this.searchInput.nativeElement.value;
     this.onSearch.emit(term);
+  }
+
+  onKeyPress(searchTerm: string): void {
+    // console.log(searchTerm);
+    this.dbouncer.next(searchTerm);
+    // this.onSearch.emit(searchTerm);
   }
 }
